b50ca9a20f272d52e634b53e03ebfbd36a518fae
SingleHe <821978332@qq.com>
2022-05-04 00:45:44 +0800

    完成了chapter04 显示微博信息

---
+++ src/cn/edu/gzy/controller/MemberServlet.java
@@ -0,0 +1,82 @@
+package cn.edu.gzy.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+@WebServlet(name="MemberServlet", urlPatterns = "/member")
+public class MemberServlet extends HttpServlet {
+    //定义微博信息的保存路径
+    private final String USERS = "D:/WorkSpace/Data/blog/users/";
+    //显示微博详情页
+    private final String MEMBER_PATH = "/showMemeber";
+    private final String LOGIN_PATH="/myblog/index.html";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req,resp);
+    }
+
+    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        //判断用户是通过记住密码登录了
+        if(req.getSession().getAttribute("login") == null){
+            resp.sendRedirect(LOGIN_PATH);//跳转回登录页面
+            return;
+        }
+        Map<Long,String> messages = messages(getUsername(req));//获取微博信息
+        req.setAttribute("message",messages);
+        req.getRequestDispatcher(MEMBER_PATH).forward(req,resp);//将数据连同请求转发给负责显示页面的servlet,来显示微博信息。
+    }
+
+
+
+    /**
+     * 获取登录用户
+     * @param req
+     * @return
+     */
+    private String getUsername(HttpServletRequest req) {
+        return (String) req.getSession().getAttribute("login");
+    }
+
+    /**
+     * 获取登录用户的微博信息，注意微博信息是保存在文件中的
+     * @param username
+     * @return
+     */
+    private Map<Long, String> messages(String username) throws IOException{
+        //1. 构造保存路径
+        Path userHome = Paths.get(USERS, username);
+        //构造保存微博信息的Map对象，为了排序，使用了TreeMap
+        //Comparator.reverseOrder是Java 8中引入的一个静态方法，它返回比较器，对对象集合进行反向自然排序。
+        Map<Long, String> messages = new TreeMap<>(Comparator.reverseOrder());
+        //显示指定目录中的所有.txt文件
+        try(DirectoryStream<Path> txts = Files.newDirectoryStream(userHome, "*.txt")){
+            for(Path txt : txts){
+                String millis = txt.getFileName().toString().replace("*.txt", "");//获得文件名，这里保存的时候，文件名用的是自1970年1月1日0时0分以来的毫秒数
+                //从文件中读取字节流，并用系统分隔符进行连接
+                String msg = Files.readAllLines(txt).stream()
+                        .collect(Collectors.joining(System.lineSeparator()));
+                messages.put(Long.parseLong(millis),msg);
+            }
+        }
+        return messages;
+    }
+
+}
---
+++ src/cn/edu/gzy/view/MemberView.java
@@ -0,0 +1,99 @@
+package cn.edu.gzy.view;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Map;
+
+@WebServlet(urlPatterns = "/showMember")
+public class MemberView extends HttpServlet {
+    private final String LOGIN_PATH = "index.html";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req,resp);
+    }
+
+    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        req.setCharacterEncoding("utf-8");
+        //1. 判断用户是否登录
+        if(req.getSession().getAttribute("login") == null){
+            resp.sendRedirect(LOGIN_PATH);
+            return;
+        }
+        String username = getUsername(req);
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset='UTF-8'>");
+        out.println("<title>新浪微博</title>");
+        out.println("<link rel='stylesheet' href='css/member.css' type='text/css'>");
+        out.println("</head>");
+        out.println("<body>");
+
+        out.println("<div class='leftPanel'>");
+        out.println("<img src='images/gzy_logo.jpg' alt='新浪微博' /><br><br>");
+        out.printf("<a href='logout'>登出 %s</a>", username);
+        out.println("</div>");
+        // 微博发送框
+        out.println("<form method='post' action='/myblog/new_message'>");
+        out.println("分享新鮮事...<br>");
+        String preMsg = req.getParameter("msg");//判断是否有预先保留用户输入的微博信息
+        if(preMsg == null){
+            preMsg = "";
+        }else{
+            out.println("<h3 style='color:red;'>信息需要在140字以内</h3>");
+        }
+        out.printf("<textarea cols='60' rows='4' name='msg'>%s</textarea><br/>",preMsg);
+        out.println("<button type='submit'>发送</button>");
+        out.println("</form>");
+        //微博发送框下的显示微博列表  使用的是table来显示
+        out.println("<table border='0' cellpadding='2' cellspacing='2'>");
+        out.println("<thead>");
+        out.println("<tr><th><hr></th></tr>");
+        out.println("</thead>");
+        out.println("<tbody>");
+        Map<Long, String> messages = (Map<Long, String>) req.getAttribute("messages");//获取从MemberServlet转发过来的微博信息
+        //循环显示微博信息,
+        messages.forEach((millis,msg)->{
+            //Instant类它代表的是时间戳,你可已使用instant.toEpochMilli()来输出Long类型的毫秒数
+            LocalDateTime dateTime = Instant.ofEpochMilli(millis).atZone(ZoneId.of("UTC+8")).toLocalDateTime();//获取东八区时区
+            out.println("<tr><td style='vertical-align:top;'>");
+            out.printf("%s<br/>",username);
+            out.printf("%s<br>",msg);
+            out.println(dateTime);
+            out.println("<form method='post' action='/myblog/delMsg'>");
+            out.printf("<input type='hidden' name='millis' value='%s'",millis);
+            out.println("<button type='submit'>删除</button>");
+            out.println("</form>");
+            out.println("<hr>");
+            out.println("</td>");
+            out.println("</tr>");
+        });
+
+
+        out.println("</tbody>");
+        out.println("</table>");
+        out.println("<hr>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    private String getUsername(HttpServletRequest req) {
+        return (String) req.getSession().getAttribute("login");
+    }
+}
